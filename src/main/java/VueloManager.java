f22c3dec5b04126bf5dc010646f9391163c3650e
Laura Manso <laura.manso@estudiantat.upc.edu>
2025-04-03 18:06:43 +0200

    parte 1, solo funciona primer test

---
+++ src/main/java/VueloManager.java
@@ -0,0 +1,32 @@
+import models.Vuelo;
+import models.Maleta;
+import models.Avion;
+
+import java.util.List;
+
+public interface VueloManager {
+    // Operaciones con aviones
+    void addAvion(String id, String modelo, String compania);
+
+    void addVuelo(String id, String horaSalida, String horaLlegada, String avionId, String origen, String destino);
+
+    void facturarMaleta(String vueloId, String userId);
+
+    void facturarMaleta(String Id, String userId, String vueloId);
+
+    List<Maleta> getMaletasByVuelo(String vueloId);
+
+    Avion getAvion(String avionId);
+
+    Vuelo getVuelo(String vueloId);
+
+    List<Vuelo> getAllVuelos();
+
+    void deleteVuelo(String vueloId);
+
+    int size();
+
+
+
+
+}
---
+++ src/main/java/models/Vuelo.java
@@ -0,0 +1,87 @@
+package models;
+
+import java.util.Date;
+
+public class Vuelo {
+
+   String id;
+   String horaSalida;
+   String horaLlegada;
+   String avionAsignado;
+   String origen;
+   String destino;
+
+
+
+    //private int capacidadMaletas;
+
+    public Vuelo() {
+    }
+
+    public Vuelo(String id, String horaSalida, String horaLlegada,
+                 String avionAsignado, String origen, String destino, int capacidadMaletas) {
+        this.id = id;
+        this.horaSalida = horaSalida;
+        this.horaLlegada = horaLlegada;
+        this.avionAsignado = avionAsignado;
+        this.origen = origen;
+        this.destino = destino;
+
+    }
+
+    public Vuelo(String id, String horaSalida, String horaLlegada, String idAvion, String origen, String destino) {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(String horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+
+    public String getHoraLlegada() {
+        return horaLlegada;
+    }
+
+    public void setHoraLlegada(String horaLlegada) {
+        this.horaLlegada = horaLlegada;
+    }
+
+    public String getAvionAsignado() {
+        return avionAsignado;
+    }
+
+    public void setAvionAsignado(String avionAsignado) {
+        this.avionAsignado = avionAsignado;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+}
+
+
+
+
+
